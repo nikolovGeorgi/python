5104c1395e8494b4964f79e8809f02fe81ff5c56
Georgi Nikolov <gnikolov@sfu.ca>
2018-01-26 11:53:28 -0600

    Added factorial stub function.

---
+++ UofS/fact.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    '''
+    Purpose:
+        Calculate the factorial of a non-negative integer
+    Pre-conditions:
+        n: non-negative integer
+    Return:
+        a non-negative integer
+    '''
+    pass
